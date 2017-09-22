264e9a5988feec686b56ebc5e8385482d9138bbf
mowenming <mowenming@outlook.com>
2017-09-23 00:26:30 +0800

    增加文件处理函数

--- ExcelDnaUdf/ExcelUDF/IOUtility.cs
+++ ExcelDnaUdf/ExcelUDF/IOUtility.cs
@@ -22,50 +22,53 @@ namespace ExcelDnaUdf
         {
 
             string[] subfolders;
-            if (Common.IsMissOrEmpty(optContainsText))
-            {
-                optContainsText = string.Empty;
-            }
-            //当isSearchAllDirectory为空或false，默认为只搜索顶层文件夹
-            if (optIsSearchAllDirectory== false)
-            {
-                subfolders = Directory.EnumerateDirectories(FileDirectory).Where(s => isContainsText(s, optContainsText)).ToArray();
-            }
-            else
-            {
+            subfolders = FileClass.GetAllFolders(FileDirectory, optContainsText, optIsSearchAllDirectory);
+            //if (Common.IsMissOrEmpty(optContainsText))
+            //{
+            //    optContainsText = string.Empty;
+            //}
+            ////当isSearchAllDirectory为空或false，默认为只搜索顶层文件夹
+            //if (optIsSearchAllDirectory== false)
+            //{
+            //    subfolders = Directory.EnumerateDirectories(FileDirectory).Where(s => isContainsText(s, optContainsText)).ToArray();
+            //}
+            //else
+            //{
 
-                subfolders = Directory.EnumerateDirectories(FileDirectory, "*", SearchOption.AllDirectories).Where(s => isContainsText(s, optContainsText)).ToArray();
-            }
+            //    subfolders = Directory.EnumerateDirectories(FileDirectory, "*", SearchOption.AllDirectories).Where(s => isContainsText(s, optContainsText)).ToArray();
+            //}
             ArrayResizer arrayResizer = new ArrayResizer();
             return arrayResizer.Resize(subfolders, isHorizontal);
 
         }
 
 
-        [ExcelFunction(Category = "文件处理", IsMacroType = true, Description = "获取指定目录下文件的文件全名")]
+        [ExcelFunction(Category = "文件处理", IsMacroType = true, Description = "获取指定文件下所有文件的全名称，可以获取子文件夹")]
         public static object GetFiles(
-                [ExcelArgument(Description = "传入的顶层目录，最终返回的结果将是此目录下的文件夹或子文件夹下的全路径文件名")] string srcFolder,
-                [ExcelArgument(Description = "查找的全路径文件名中是否需要包含指定字符串，不传参数默认为返回所有文件夹，可传入复杂的正则表达式匹配。")] string containsText,
-                [ExcelArgument(Description = "是否查找顶层目录下的文件夹的所有子文件夹，TRUE和非0的字符或数字为搜索子文件夹，其他为否，不传参数时默认为否")] object isSearchAllDirectory,
-                [ExcelArgument(Description = "返回的结果是按按列排列还是按行排列，传入L按列排列，传入H按行排列，不传参数或传入非L或H则默认按列排列")] string optAlignHorL)
+                [ExcelArgument(Description = "指定目录")] string strFolder,
+                [ExcelArgument(Description = @"正则指定搜索条件,0为搜索xlsx文件，\S\.xlsx$ 表示")] string strRegesText,
+                [ExcelArgument(Description = "是否搜索子文件夹,1搜索，0不搜索。默认不搜索")] bool blIsSearchSubfolder,
+                [ExcelArgument(Description = "返回的结果是按【行】排列，true按行，false按列，默认按列")] bool blIsHorizontal)
         {
             string[] files;
-            if (Common.IsMissOrEmpty(containsText))
-            {
-                containsText = string.Empty;
-            }
-            //当isSearchAllDirectory为空或false，默认为只搜索顶层文件夹
-            if (Common.IsMissOrEmpty(isSearchAllDirectory) || Common.TransBoolPara(isSearchAllDirectory) == false)
-            {
-                files = Directory.EnumerateFiles(srcFolder).Where(s => isContainsText(s, containsText)).ToArray();
-            }
-            else
-            {
+            files = FileClass.GetAllFiles(strFolder, strRegesText, blIsSearchSubfolder);
+            //if (Common.IsMissOrEmpty(containsText))
+            //{
+            //    containsText = string.Empty;
+            //}
+            ////当isSearchAllDirectory为空或false，默认为只搜索顶层文件夹
+            //if (Common.IsMissOrEmpty(isSearchAllDirectory) || Common.TransBoolPara(isSearchAllDirectory) == false)
+            //{
+            //    files = Directory.EnumerateFiles(srcFolder).Where(s => isContainsText(s, containsText)).ToArray();
+            //}
+            //else
+            //{
 
-                files = Directory.EnumerateFiles(srcFolder, "*", SearchOption.AllDirectories).Where(s => isContainsText(s, containsText)).ToArray();
-            }
+            //    files = Directory.EnumerateFiles(srcFolder, "*", SearchOption.AllDirectories).Where(s => isContainsText(s, containsText)).ToArray();
+            //}
             ArrayResizer arrayResizer = new ArrayResizer();
-            return arrayResizer.Resize(Common.ReturnDataArray(files, optAlignHorL));
+            //return arrayResizer.Resize(Common.ReturnDataArray(files, optAlignHorL));
+            return arrayResizer.Resize(files,blIsHorizontal);
         }
 
         private static bool isContainsText(string s, string containstext)
---
+++ ExcelDnaUdf/PlClass/FileClass.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    //=GetFiles(A1,"\S\.xlsx$",TRUE,"L")
+   public static class FileClass
+    {
+    /// <summary>
+    /// 获取指定文件下所有【文件的全名称】，
+    /// 可以获取子文件夹
+    /// </summary>
+    /// <param name="strFolder">指定目录</param>
+    /// <param name="strRegesText">正则指定搜索条件,0为搜索xlsx文件，"\S\.xlsx$"表示 </param>
+    /// <param name="blIsSearchSubfolder">是否搜索子文件夹,1搜索，0不搜索。默认不搜索</param>
+    /// <returns>返回搜索到的文件全名数组</returns>
+    public static string[] GetAllFiles( string strFolder, string strRegesText, bool blIsSearchSubfolder=false)
+    {
+        string[] files;
+        if (strRegesText==null | strRegesText=="0")
+        {
+            strRegesText = @"\S\.xlsx$";
+        }
+
+        if(blIsSearchSubfolder)//搜索子文件夹
+        {
+            files = Directory.EnumerateFiles(strFolder, "*", SearchOption.AllDirectories).Where(s => isContainsText(s, strRegesText)).ToArray();
+        }
+        else
+        {
+            files = Directory.EnumerateFiles(strFolder).Where(s => isContainsText(s, strRegesText)).ToArray();
+        }
+
+        return files;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="strsFiles"></param>
+    /// <param name="existExcelFileNames"></param>
+    /// <param name="dic"></param>
+    /// <returns></returns>
+    public static bool FilesWithSameName(string[] strsFiles,out List<string> existExcelFileNames,out Dictionary<string, List<string>> dic)
+    {
+        existExcelFileNames = null;
+        dic = null;
+        foreach(string str in strsFiles)
+        {
+            string strP = Path.GetFileNameWithoutExtension(str);
+            if (dic.ContainsKey(strP))//存在该key
+            {
+                dic[strP].Add(str);
+            }
+            else
+            {
+                dic.Add(strP, new List<string> { str });
+            }
+            existExcelFileNames.Add(strP);//不带扩展名的文件名称，如item
+        }
+        return false;
+    }
+    /// <summary>
+    /// 获取指定文件下所有【文件夹】的名称，
+    /// 可以获取子文件夹
+    /// </summary>
+    /// <param name="strFolder">指定目录</param>
+    /// <param name="strRegesText">正则指定搜索条件 </param>
+    /// <param name="blIsSearchSubfolder">是否搜索子文件夹,1搜索，0不搜索。默认不搜索</param>
+    /// <returns>返回搜索到的文件全名数组</returns>
+    public static string[] GetAllFolders(string strFolder, string strRegesText, bool blIsSearchSubfolder = false)
+    {
+        string[] files;
+        if (string.IsNullOrEmpty(strRegesText.ToString().Trim()))
+        {
+            strRegesText = string.Empty;
+
+        }
+
+        if (blIsSearchSubfolder)//搜索子文件夹
+        {
+            files = Directory.EnumerateDirectories(strFolder, "*", SearchOption.AllDirectories).Where(s => isContainsText(s, strRegesText)).ToArray();
+        }
+        else
+        {
+            files = Directory.EnumerateDirectories(strFolder).Where(s => isContainsText(s, strRegesText)).ToArray();
+        }
+
+        return files;
+    }
+    private static bool isContainsText(string s, string containstext)
+    {
+        if (string.IsNullOrEmpty(containstext))
+        {
+            return true;
+        }
+        else
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(s, containstext);
+        }
+
+    }
+}
+
